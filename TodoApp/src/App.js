c8cd05a64cbc03054ed0207bf00fdbe875b1e969
anshi-j <anshikajaiswar1106@gmail.com>
2023-10-19 17:19:16 +0530

    todo

---
+++ TodoApp/src/App.js
@@ -0,0 +1,15 @@
+import { Form } from "./Components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h1>TO-DO APP</h1>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
